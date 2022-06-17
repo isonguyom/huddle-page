556ddbfd01808c5ee60cb400f966aa440b9a1f9b
isonguyom <isonguyom@gmail.com>
2022-06-17 10:46:49 +0100

    add responsiveness

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// let regForm = document.getElementsByTagName("form");
-// let submitBtn = document.getElementById("submitBtn");
-
-
-// submitBtn.addEventListener("submit", function() {
-//     regForm.attribute.v
-// })
